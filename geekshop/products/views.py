7e0f56b1c2ac7e83e2140161ac776402c6fcefba
prapeller <prapeller@mail.ru>
2021-09-15 13:48:00 +0300

    added /test view, url

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,8 +1,43 @@
+import datetime
+from djmoney.money import Money
+
 from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
     return render(request, 'products/index.html')
 
+
 def products(request):
     return render(request, 'products/products.html')
+
+
+def test(request):
+    context = {
+        'title': 'geekshop',
+        'header': 'Welcome to our website!',
+        'user': 'Pablo',
+        'time': datetime.datetime.now(),
+        'products': [
+            {'id': 1,
+             'name': 'Худи черного цвета с монограммами adidas Originals',
+             'price': Money(6090.20, 'RUB')
+             },
+            {'id': 2,
+             'name': 'Синяя куртка The North Face',
+             'price': Money(23725.50, 'RUB')
+             },
+        ],
+        'promo_products': [
+            {'id': 1,
+             'name': 'Snickers Nike',
+             'price': Money(990, 'RUB')
+             },
+            {'id': 2,
+             'name': 'T-shirt ABIBAS',
+             'price': Money(990, 'RUB')
+             }
+        ]
+    }
+    return render(request, 'products/test.html', context=context)
