f905de102c27bb75691b3f44b02dba013b1c5d96
prapeller <prapeller@mail.ru>
2021-09-24 13:27:35 +0300

    finished less_04

---
+++ geekshop/users/forms.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.forms import forms, AuthenticationForm, UserCreationForm
+
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    # username = forms.CharField(
+    #     widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Enter your username'}))
+    # password = forms.CharField(
+    #     widget=forms.PasswordInput(attrs={'class': 'form-control py-4', 'placeholder': 'Enter your password'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['placeholder'] = 'Enter your username'
+        self.fields['password'].widget.attrs['placeholder'] = 'Enter your password'
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['placeholder'] = 'Enter your username'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter your email'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter your first name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter your last name'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Enter your password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Repeate your password'
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+    def clean(self):
+        cleaned_data = super(self.__class__, self).clean()
+        if 'pupkin' or 'vasya' in (
+                self.cleaned_data.get('username').lower(),
+                self.cleaned_data.get('email').lower(),
+                self.cleaned_data.get('first_name').lower(),
+                self.cleaned_data.get('last_name').lower()
+        ):
+            raise forms.ValidationError("VASYA PUPKIN in da house!!! Get rid of that motherf...er!!!")
+        return cleaned_data
--- geekshop/users/views.py
+++ geekshop/users/views.py
@@ -1,14 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm, UserRegisterForm
+
 
 # Create your views here.
 def login(request):
+    form = UserLoginForm()
+    errors = {}
+
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            errors = form.errors.as_data()
+
     context = {
-        'title': 'Geekshop login'
+        'title': 'Geekshop login',
+        'form': form,
+        'errors': errors
     }
     return render(request, 'users/login.html', context)
 
+
 def register(request):
+    form = UserRegisterForm()
+    errors = {}
+
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users_app:login'))
+        else:
+            errors = form.errors.as_data()
+
     context = {
-        'title': 'Geekshop register'
+        'title': 'Geekshop register',
+        'form': form,
+        'errors': errors
     }
     return render(request, 'users/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
