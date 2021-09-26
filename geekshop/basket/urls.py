1906fe856d47b0ae13dadaad716a7bb784d2b01e
prapeller <prapeller@mail.ru>
2021-09-26 15:22:27 +0300

    added 2 routes for adding and removing basket products

--- geekshop/basket/urls.py
+++ geekshop/basket/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from basket.views import add_product, remove_product
 
 app_name = 'basket'
 
 urlpatterns = [
-    # path('', products, name='main'),
+    path('add_product<int:product_id>/', add_product, name='add_product'),
+    path('remove_product<int:basket_product_id>/', remove_product, name='remove_product'),
 ]
