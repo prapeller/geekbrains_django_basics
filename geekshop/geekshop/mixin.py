2fcf94d24d8365588e3ecbcd85b84dc2794b4718
prapeller <prapeller@mail.ru>
2021-10-07 16:23:19 +0300

    added title context mixin

--- geekshop/geekshop/mixin.py
+++ geekshop/geekshop/mixin.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.generic.base import ContextMixin
 
 
 class IsStuffDispatchMixin(View):
@@ -13,3 +14,12 @@ class IsSuperuserDispatchMixin(View):
     @method_decorator(user_passes_test(lambda u: u is not None and u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
         return super(IsSuperuserDispatchMixin, self).dispatch(request, *args, **kwargs)
+
+
+class TitleContextMixin(ContextMixin):
+    title = ''
+
+    def get_context_data(self, **kwargs):
+        context = super(TitleContextMixin, self).get_context_data(**kwargs)
+        context['title'] = self.title
+        return context
