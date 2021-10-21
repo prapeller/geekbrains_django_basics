db9e69150016d54e4be1274aece11b5a6dafea3e
prapeller <prapeller@mail.ru>
2021-10-21 17:55:30 +0300

    added explicit backend to verify fbv

--- geekshop/users/views.py
+++ geekshop/users/views.py
@@ -165,7 +165,7 @@ def verify(request, email: str, activation_key: str):
             user.activation_key_created = None
             user.is_active = True
             user.save()
-            auth.login(request, user)
+            auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
         return render(request, 'users/verification.html')
     except Exception as e:
         print(e)
