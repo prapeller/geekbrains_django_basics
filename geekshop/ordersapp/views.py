3bb4a759362e90164d8d08e8889514ef4bef2be7
prapeller <prapeller@mail.ru>
2021-10-22 17:39:40 +0300

    corrected ordersapp views

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponseRedirect
+
 from basket.models import Basket
 from ordersapp.forms import OrderProductForm
 from ordersapp.models import Order, OrderProduct
@@ -5,9 +7,9 @@ from geekshop.mixin import TitleContextMixin
 
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, \
     DetailView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.shortcuts import get_object_or_404
-from django.forms import inlineformset_factory
+from django.forms import inlineformset_factory, modelformset_factory
 from django.db import transaction
 
 
@@ -22,37 +24,29 @@ class OrderCreate(CreateView, TitleContextMixin):
     model = Order
     title = 'Geekshop | Create Order'
     fields = []
-    success_url = reverse_lazy('orders_app:order_list')
-    template_name = 'ordersapp/order_create.html'
+    template_name_suffix = '_create'
 
     def get_context_data(self, **kwargs):
-        context = super(OrderCreate, self).get_context_data(**kwargs)
-
-        OrdProdFormSet = inlineformset_factory(Order, OrderProduct,
-                                               form=OrderProductForm,
-                                               extra=1)
-
+        context = super().get_context_data(**kwargs)
+        ord_products = Basket.objects.filter(user=self.request.user)
+        OrdProdFormSet = inlineformset_factory(Order, OrderProduct, form=OrderProductForm,
+                                               extra=ord_products.count())
+        formset = OrdProdFormSet()
         if self.request.POST:
             formset = OrdProdFormSet(self.request.POST)
 
-        else:
-            bskt_prod_qrst = Basket.objects.filter(user=self.request.user)
+        for i, form in enumerate(formset.forms):
+            product = ord_products[i].product
+            quantity = Basket.objects.filter(product_id=product.id,
+                                             user_id=self.request.user.id).first().quantity
+            price = product.price
 
-            if bskt_prod_qrst:
-                OrdProdFormSet = inlineformset_factory(Order, OrderProduct,
-                                                       form=OrderProductForm,
-                                                       extra=bskt_prod_qrst.count())
-                formset = OrdProdFormSet()
-
-                for i, form in enumerate(formset.forms):
-                    form.initial['product'] = bskt_prod_qrst[i].product
-                    form.initial['quantity'] = bskt_prod_qrst[i].product.quantity
-                    form.initial['price'] = bskt_prod_qrst[i].product.price
-
-            else:
-                formset = OrdProdFormSet()
+            form.initial['product'] = product
+            form.initial['quantity'] = quantity
+            form.initial['price'] = price
 
         context['formset'] = formset
+
         return context
 
     def form_valid(self, form):
@@ -66,32 +60,28 @@ class OrderCreate(CreateView, TitleContextMixin):
                 formset.instance = self.object
                 formset.save()
 
-            if self.object.get_total_price() == 0:
-                self.object.delete()
+        if self.object.get_total_quantity() == 0:
+            self.object.delete()
 
-        return super(OrderCreate, self).form_valid(form)
+        return super().form_valid(form)
 
 
 class OrderUpdate(UpdateView, TitleContextMixin):
     model = Order
     title = 'Geekshop | Update Order'
     fields = []
-    success_url = reverse_lazy('orders_app:order_list')
-    template_name = 'ordersapp/order_update.html'
+    template_name_suffix = '_update'
 
     def get_context_data(self, **kwargs):
-        context = super(OrderUpdate, self).get_context_data(**kwargs)
-
+        context = super().get_context_data(**kwargs)
         OrdProdFormSet = inlineformset_factory(Order, OrderProduct, form=OrderProductForm, extra=1)
-
+        formset = OrdProdFormSet(instance=self.object)
         if self.request.POST:
             formset = OrdProdFormSet(self.request.POST, instance=self.object)
 
-        else:
-            formset = OrdProdFormSet(instance=self.object)
-            for form in formset:
-                if form.instance.pk:
-                    form.initial['price'] = form.instance.product.price
+        for form in formset.forms:
+            if form.instance.pk:
+                form.initial['price'] = form.instance.product.price
 
         context['formset'] = formset
         return context
@@ -101,16 +91,16 @@ class OrderUpdate(UpdateView, TitleContextMixin):
         formset = context['formset']
 
         with transaction.atomic():
-            form.instance.user = self.request.user
+            # form.instance.user = self.request.user
             self.object = form.save()
             if formset.is_valid():
                 formset.instance = self.object
                 formset.save()
 
-            if self.object.get_products() == 0:
-                self.object.delete()
+        if self.object.get_total_quantity() == 0:
+            self.object.delete()
 
-        return super(self.__class__, self).form_valid(form)
+        return super(OrderUpdate, self).form_valid(form)
 
 
 class OrderDelete(DeleteView):
@@ -127,4 +117,4 @@ def order_send_to_process(request, pk):
     order = get_object_or_404(Order, pk=pk)
     order.status = Order.SENT_TO_PROCESSING
     order.save()
-    return reverse_lazy('orders_app:order_list')
+    return HttpResponseRedirect(reverse('orders_app:order_list'))
