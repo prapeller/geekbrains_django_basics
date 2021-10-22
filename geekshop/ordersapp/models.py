26fe0af36065ab197f69541037e5bcd9deb83f05
prapeller <prapeller@mail.ru>
2021-10-22 17:40:07 +0300

    corrected ordersapp models

--- geekshop/ordersapp/models.py
+++ geekshop/ordersapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 from products.models import Product
 
@@ -29,26 +30,31 @@ class Order(models.Model):
     is_active = models.BooleanField(verbose_name='is active', default=True)
 
     def __str__(self):
-        return f'order id: {self.pk}, product: {list(self.order_products)}'
+        return f'Order id: {self.pk} from {self.created}, last update {self.updated}'
+
+    def get_absolute_url(self):
+        return reverse('orders_app:order_list')
 
     def get_total_quantity(self):
-        products = self.order_products.select_related()
-        return sum(list(map(lambda product: product.quantity, products)))
+        ord_products = self.order_products.select_related()
+        return sum(list(map(lambda ord_product: ord_product.quantity, ord_products)))
 
     def get_total_price(self):
-        products = self.order_products.select_related()
-        return sum(list(map(lambda product: product.get_subtotal(), products)))
+        ord_products = self.order_products.select_related()
+        return sum(list(map(lambda ord_product: ord_product.get_subtotal(), ord_products)))
 
     def get_products(self):
-        products = self.order_products.select_related()
-        return products
+        order_products = self.order_products.select_related()
+        return order_products
 
     def delete(self, using=None, keep_parents=False):
-        for product in self.order_products.select_related():
-            product.quantity += product.quantity
-            product.save()
+        ord_products = self.order_products.select_related()
+        for ord_product in ord_products:
+            ord_product.product.quantity += ord_product.quantity
+            ord_product.product.save()
         self.is_active = False
         self.save()
+
 
 
 class OrderProduct(models.Model):
